aa0c70ebad4c3def3f6275fa609021e0884afba9
fedex2018 <federico@grayshirts.com.ar>
2019-05-24 22:41:16 -0300

    first commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+        System.out.println("-------------------PROBANDO---------------------");
+
+        System.out.println("-------------------PROBANDO---------------------");
+
+        System.out.println("-------------------PROBANDO---------------------");
+
+        System.out.println("-------------------PROBANDO---------------------");
+
+        SpringApplication.run(DemoApplication.class, args);
+	}
+}
