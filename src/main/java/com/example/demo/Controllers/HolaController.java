ee0ced972bc2556809f14598b6ef493e8c6b0756
fedex2018 <federico@grayshirts.com.ar>
2019-05-24 22:58:13 -0300

    reorganization of files

--- src/main/java/com/example/demo/Controllers/HolaController.java
+++ src/main/java/com/example/demo/Controllers/HolaController.java
@@ -1,6 +1,8 @@
-package com.example.demo;
+package com.example.demo.Controllers;
 
 import java.util.concurrent.atomic.AtomicLong;
+
+import com.example.demo.Models.Greeting;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
--- src/main/java/com/example/demo/Models/Greeting.java
+++ src/main/java/com/example/demo/Models/Greeting.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.demo.Models;
 
 public class Greeting {
 
